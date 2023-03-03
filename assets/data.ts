6d24492f479199c111d7152bd42faf0f461a1b6d
J0808 <jacksondaniel0909@gmail.com>
2023-03-03 10:18:56 +0530

    Auto-generated commit [ci skip]

---
+++ assets/data.ts
@@ -0,0 +1,47 @@
+export const data = [
+    {title: "Trivandrum",
+image:"d1.jpg"},
+
+    {title: "Kollam",
+image:"d2.jpg"},
+
+    {title: "Pta",
+    image:"d5.jpg"},
+
+    {title: "Alappuzha",
+    image:"d3.jpg"},
+
+    {title: "Kottayam",
+    image:"d7.jpg"},
+
+    {title: "Idukki",
+    image:"d11.jpg"},
+
+    {title: "Ernakulam",
+    image:"d10.jpg"},
+
+    {title: "Thrissur",
+    image:"d6.jpg"},
+
+    {title: "Palakkad",
+    image:"d2.jpg"},
+
+    {title: "Malappuram",
+    image:"d5.jpg"},
+
+    {title: "Kozhikode",
+    image:"d8.jpg"},
+
+    {title: "Wayanad",
+    image:"d9.jpg"},
+
+    {title: "Kannur",
+    image:"d10.jpg"},
+    
+    {title: "Kasaragod",
+    image:"d4.jpg"}
+    
+
+    
+
+]
